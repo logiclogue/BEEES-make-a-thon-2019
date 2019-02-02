3a65b759f6147e106cfc06673e80ab7132a6fdcf
jerguslejko <jergus.lejko@gmail.com>
2019-02-02 18:03:20 +0000

    stuff

--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
-var express = require('express');  
-var app = express();  
-var server = require('http').createServer(app);  
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-app.use(express.static(__dirname + '/../client'));  
+app.use(express.static(__dirname + '/../client'));
 
-app.get('/', function(req, res,next) {  
+app.get('/', function(req, res, next) {
     res.sendFile(__dirname + '/../client/index.html');
 });
 
-io.on("connection", client => {
-    console.log("CONNECTED!!!");
+io.on('connection', client => {
+    console.log('CONNECTED!!!');
+
+    client.on('face', x => {
+        console.log(x);
+    });
 });
 
 server.listen(3000);
